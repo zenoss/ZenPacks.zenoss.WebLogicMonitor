927df9ff8e78568b70262868ce225db394afad11
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-06-05 19:21:06 +0000

    refs #613  * fixing compat_zenoss_version
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.WebLogicMonitor@1819 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 INSTALL_REQUIRES = ['ZenPacks.zenoss.ZenJMX']
-COMPAT_ZENOSS_VERS = '2.2'
+COMPAT_ZENOSS_VERS = '>= 2.2'
 PREV_ZENPACK_NAME = 'WebLogicMonitor'
 # STOP_REPLACEMENTS
 ################################
